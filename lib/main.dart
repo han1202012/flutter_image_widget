0caec22dda81329d1ac9990fb35b6c2bbaa93b8e
han1202012 <octopus_truth@163.com>
2021-03-24 21:36:14 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(MyApp());
 
@@ -65,6 +66,32 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView(
           children: [
 
+
+            Center(
+              // 加载自定义图标
+              child: Icon(IconData(0xe855, fontFamily: "MyFlutterApp",), size: 150,),
+            ),
+
+            Center(
+              // 加载 Flutter 内置图标
+              child: Icon(Icons.threed_rotation, size: 200,),
+            ),
+
+
+            Stack(
+              children: [
+                Center(
+                  // 网络加载时显示本地的资源图片
+                  child: FadeInImage.assetNetwork(
+                    // Placeholder
+                    placeholder: "images/waiting.gif",
+                    image: "https://img-blog.csdnimg.cn/20210324110914742.png",
+                  ),
+                )
+              ],
+            ),
+
+
             Stack(
               children: [
                 // 进度条
@@ -80,8 +107,22 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
 
+
+            Center(
+              // 图片加载完成之前显示的是 placeholder , 加载完成后显示网络图片
+              child: CachedNetworkImage(
+                // 加载网络图片过程中显示的内容 , 这里显示进度条
+                placeholder: (context, url)=>CircularProgressIndicator(),
+                // 网络图片地址
+                imageUrl: "https://img-blog.csdnimg.cn/20210324100419204.png",
+              ),
+            ),
+
+
+
+
             // 图片组件 , 从网络中加载一张图片
-            /*Image.network(
+            Image.network(
               // 图片地址
               "https://img-blog.csdnimg.cn/2021032313493741.png",
             ),
@@ -90,14 +131,14 @@ class _MyHomePageState extends State<MyHomePage> {
               image: AssetImage("images/sidalin.png"),
             ),
 
-            Image.asset('images/sidalin2.png', ),
+            //Image.asset('images/sidalin2.png', ),
 
             /// 从 SD 卡加载图片
             if(sdPath != null)
               Image.file(
                 File('$sdPath/sidalin3.png'),
                 width: 200,
-              ),*/
+              ),
           ],
         )
 
